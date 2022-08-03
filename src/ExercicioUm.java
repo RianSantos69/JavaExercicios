d9bd5f3c178e981eba160d12cd771fa0b885c549
RianSantos69 <104446066+RianSantos69@users.noreply.github.com>
2022-08-03 20:53:17 -0300

    Add files via upload

--- src/ExercicioUm.java
+++ src/ExercicioUm.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.*;
+import java.text.*;
 
 public class ExercicioUm {
 
@@ -30,43 +32,207 @@ public class ExercicioUm {
 
         switch (menu) {
             // 1) Crie um programa que calcule a média (`(nota1 + nota2 + nota3 / 3)`)
-            case 1:
-                // media(double notaUm, double notaDois, double notaTres)
+            case 1:{
+                //media
+                int nota1 = 10;
+                int nota2 = 5;
+                int nota3 = 7;
+                double media = (double) (nota1 + nota2 + nota3) / 3;
+    
+                System.out.println("Média");
+                System.out.println("Primeira Nota: " + nota1);
+                System.out.println("Segunda Nota: " + nota2);
+                System.out.println("Terceira Nota: " + nota3);
+    
+                System.out.println("Média das Notas: " + media);
+    
+               
+    
+                Date relogio = new Date();
+                DateFormat dhf = new SimpleDateFormat("dd/MM/yyyy");
+                //Locale BRAZIL = new Locale("pt","BR");
+                String s;
+        
+                dhf = DateFormat.getDateTimeInstance(); 
+                s = dhf.format(relogio);
+                System.out.println("Data : " + s);
+                //System.out.println("Relogio: ");
+                //System.out.println(relogio.toString());
+        
+                
+                
+            
+            
                 break;
+            }
+
             // 2) Crie um programa que calcule a área (`lado1 * lado2`)
-            case 2:
-                // area(double ladoUm, double ladoDois)
+            case 2:{
+                
+                 //area
+                 int lado1 = 5;
+                 int lado2 = 7;
+                 int area = lado1 * lado2;
+     
+                 System.out.println("Lado 1: " + lado1);
+                 System.out.println("Lado 2: " + lado2);
+                 System.out.println("Area: " + area);
+
+                 Date relogio = new Date();
+                DateFormat dhf = new SimpleDateFormat("dd/MM/yyyy");
+                //Locale BRAZIL = new Locale("pt","BR");
+                String s;
+        
+                dhf = DateFormat.getDateTimeInstance(); 
+                s = dhf.format(relogio);
+                System.out.println("Data : " + s);
                 break;
+            }
             // 3) Crie um programa que calcule o volume (`largura * altura * profundidade`)
-            case 3:
+            case 3:{
+
+                double largura = 20;
+                double altura = 10;
+                double profundidade = 5;
+                double volume = largura * altura * profundidade;
+
+                System.out.println("");
+                System.out.println("Largura: " + largura);
+                System.out.println("Altura: " + altura);
+                System.out.println("Profundidade: " + profundidade);
+                System.out.println("Volume: " + volume);
+
+
+                Date relogio = new Date();
+                DateFormat dhf = new SimpleDateFormat("dd/MM/yyyy");
+                //Locale BRAZIL = new Locale("pt","BR");
+                String s;
+        
+                dhf = DateFormat.getDateTimeInstance(); 
+                s = dhf.format(relogio);
+                System.out.println("Data : " + s);
+            }
                 // volume(double largura, double altura, double profundidade)
                 break;
             // 4) Crie um programa que avalie se um valor é maior do que o dobro de outro valor
-            case 4:
+            case 4:{
+
+
+                double n1 = 3;
+                double n2 = 3.5;
+                double maior = 0;
+
+                System.out.println("Número 1 = " + n1);
+                System.out.println("Número 2 = " + n2);
+                if(n1>n2){
+                    maior = n1;
+                    System.out.println("Maior Número: " + maior);
+                }else{
+                    maior = n2;
+                    System.out.println("Maior Número: " + maior);
+                }
+                
+
+                Date relogio = new Date();
+                DateFormat dhf = new SimpleDateFormat("dd/MM/yyyy");
+                //Locale BRAZIL = new Locale("pt","BR");
+                String s;
+        
+                dhf = DateFormat.getDateTimeInstance(); 
+                s = dhf.format(relogio);
+                System.out.println("Data : " + s);
+
+            }
                 // valorMaiorQueDobro(double base, double verificar)
                 break;
             // 5) Crie um programa que calcule _bhaskara_
-            case 5:
+            case 5:{
+
+
+                Date relogio = new Date();
+                DateFormat dhf = new SimpleDateFormat("dd/MM/yyyy");
+                //Locale BRAZIL = new Locale("pt","BR");
+                String s;
+                
+                dhf = DateFormat.getDateTimeInstance(); 
+                s = dhf.format(relogio);
+                System.out.println("Data : " + s);
+            }
                 // bhaskara(int a, int b, int c);
                 break;
             // 6) Crie um programa que calcule a velocidade média de uma viagem (`distancia (km) / tempo (h)`)
-            case 6:
+            case 6:{
+
+
+                Date relogio = new Date();
+                DateFormat dhf = new SimpleDateFormat("dd/MM/yyyy");
+                //Locale BRAZIL = new Locale("pt","BR");
+                String s;
+        
+                dhf = DateFormat.getDateTimeInstance(); 
+                s = dhf.format(relogio);
+                System.out.println("Data : " + s);
+            }
                 // mediaViagem(double distancia, double tempo)
                 break;
             // 7) Crie um programa que calcule o percentual de imposto pago (`faturamento \ qtd imposto`)
-            case 7:
+            case 7:{
+
+
+                Date relogio = new Date();
+                DateFormat dhf = new SimpleDateFormat("dd/MM/yyyy");
+                //Locale BRAZIL = new Locale("pt","BR");
+                String s;
+        
+                dhf = DateFormat.getDateTimeInstance(); 
+                s = dhf.format(relogio);
+                System.out.println("Data : " + s);
+            }
                 // percentualImposto(double faturamento, double qtdImposto)
                 break;
             // 8) Crie um programa que teste se um valor é par ou ímpar
-            case 8:
+            case 8:{
+
+
+                Date relogio = new Date();
+                DateFormat dhf = new SimpleDateFormat("dd/MM/yyyy");
+                //Locale BRAZIL = new Locale("pt","BR");
+                String s;
+        
+                dhf = DateFormat.getDateTimeInstance(); 
+                s = dhf.format(relogio);
+                System.out.println("Data : " + s);
+            }
                 // parOuImpar(int valor)
                 break;
             // 9) Crie um programa que compare dois valores Strings
-            case 9:
+            case 9:{
+
+
+                Date relogio = new Date();
+                DateFormat dhf = new SimpleDateFormat("dd/MM/yyyy");
+                //Locale BRAZIL = new Locale("pt","BR");
+                String s;
+        
+                dhf = DateFormat.getDateTimeInstance(); 
+                s = dhf.format(relogio);
+                System.out.println("Data : " + s);
+            }
                 // compareString(String stringUm, String stringDois)
                 break;
             // 10) Crie um programa que tenha uma variável com ponto em String e converta seu valor para inteiro
-            case 10:
+            case 10:{
+
+
+                Date relogio = new Date();
+                DateFormat dhf = new SimpleDateFormat("dd/MM/yyyy");
+                //Locale BRAZIL = new Locale("pt","BR");
+                String s;
+        
+                dhf = DateFormat.getDateTimeInstance(); 
+                s = dhf.format(relogio);
+                System.out.println("Data : " + s);
+            }
                 // convertString(String valor);
                 break;
             // 11) Crie um programa que receba o salário e diga o valor de imposto de renda conforme abaixo:
@@ -77,11 +243,33 @@ public class ExercicioUm {
             // De 2.826,66 até 3.751,05 | 15%
             // De 3.751,06 até 4.664,68 | 22,5%
             // Acima de 4.664,68        | 27,5%
-            case 11:
+            case 11:{
+
+
+              Date relogio = new Date();
+                DateFormat dhf = new SimpleDateFormat("dd/MM/yyyy");
+                //Locale BRAZIL = new Locale("pt","BR");
+                String s;
+        
+                dhf = DateFormat.getDateTimeInstance(); 
+                s = dhf.format(relogio);
+                System.out.println("Data : " + s);  
+            }
                 // percentualImpostoSalario(double salario);
                 break;
             // DESAFIO - Crie um programa que teste se um número é primo
-            case 12:
+            case 12:{
+
+
+               Date relogio = new Date();
+                DateFormat dhf = new SimpleDateFormat("dd/MM/yyyy");
+                //Locale BRAZIL = new Locale("pt","BR");
+                String s;
+        
+                dhf = DateFormat.getDateTimeInstance(); 
+                s = dhf.format(relogio);
+                System.out.println("Data : " + s); 
+            }
                 // testePrimo(int valor)
                 break;
             default:
